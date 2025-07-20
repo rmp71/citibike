b3515790fe8bd9f255ae6672b8ed89f57b0fa0b6
Riya Patel <riyamp71@gmail.com>
2025-07-20 03:17:11 +0000

    updated dim_stations table to have city_id in there as well

--- models/refined/dim_station.sql
+++ models/refined/dim_station.sql
@@ -13,12 +13,23 @@ with
             end_station_latitude as station_latitude,
             end_station_longitude as station_longitude
         from {{ ref("citibike_trips") }}
+    ),
+    city as (select distinct lat, lon, city_id from {{ ref("weather") }}),
+    city_join as (
+        select distinct s.*, c.city_id
+        from stations s
+        left join city c on (s.station_latitude = c.lat and s.station_longitude = c.lon)
     )
 select
     {{
         dbt_utils.generate_surrogate_key(
-            ["station_id", "station_name","station_latitude", "station_longitude"]
+            [
+                "station_id",
+                "station_name",
+                "station_latitude",
+                "station_longitude",
+                "city_id",
+            ]
         )
-    }} as dim_station_key,
-    *
-from stations
+    }} as dim_station_key, *
+from city_join
